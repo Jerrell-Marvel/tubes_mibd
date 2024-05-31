e630eef90e9e03979619e0a6761de4a33cb6b222
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-05-31 23:05:32 +0700

    create updateFurnitur function

--- services/Furnitur.py
+++ services/Furnitur.py
@@ -1,6 +1,6 @@
 from connectDB import conn
 
-def insertFurnitur(nama , deskripsi, url_foto_display, cursor=None):
+def insertFurnitur(nama, deskripsi, url_foto_display, cursor=None):
     query = '''
         INSERT INTO Furnitur (nama, deskripsi, url_foto_display)
         VALUES
@@ -8,14 +8,30 @@ def insertFurnitur(nama , deskripsi, url_foto_display, cursor=None):
     '''
     cursor.execute(query, (nama, deskripsi, url_foto_display))
 
-def insertBagianFurnitur(nama, panjang, lebar, tinggi, id_furnitur, cursor=None) :
-    query = '''
-        INSERT INTO Bagian_Furnitur (nama, panjang, lebar, tinggi, id_furnitur)
-        VALUES
-        (?, ?, ?, ?, ?)
-    '''
+
+def updateFurnitur(id_furnitur, nama=None, deskripsi=None, url_foto_display=None, cursor=None):
+    query = '''UPDATE Furnitur SET '''
+
+    colValue = []
+    if nama is not None :
+        query += f"nama=?, "
+        colValue.append(nama)
     
-    cursor.execute(query, (nama, panjang, lebar, tinggi, id_furnitur))
+    if deskripsi is not None :
+        query += f"deskripsi=?, "
+        colValue.append(deskripsi)
+
+    if url_foto_display is not None :
+        query += f"url_foto_display=?"
+        colValue.append(url_foto_display)
+    
+    query = query.rstrip(", ")
+    
+    colValue.append(id_furnitur)
+    query += " WHERE id_furnitur = ?"
+
+    cursor.execute(query, colValue)
+
 
 
 
