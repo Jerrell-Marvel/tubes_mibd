ee5436468c971e801cfa5050cc59fc8794046e89
Frox8 <91016925+Frox8@users.noreply.github.com>
2024-06-01 23:57:51 +0700

    feat: add login pengguna

--- services/Pengguna.py
+++ services/Pengguna.py
@@ -1,5 +1,3 @@
-from connectDB import conn
-
 def insertPengguna(username, email, nama, password, nomor_telepon, alamat, id_kelurahan, cursor=None):
     query = '''
         INSERT INTO Pengguna (username, email, nama, password, nomor_telepon, alamat, id_kelurahan)
@@ -7,3 +5,22 @@ def insertPengguna(username, email, nama, password, nomor_telepon, alamat, id_ke
         (?, ?, ?, ?, ?, ?, ?);
     '''
     cursor.execute(query, (username, email, nama, password, nomor_telepon, alamat, id_kelurahan))
+    
+def getPenggunaByUsername(username,cursor):
+    query = '''
+        SELECT id_pengguna, username, password, role FROM Pengguna 
+        WHERE LOWER(username) = LOWER(?) 
+    '''
+    pengguna = cursor.execute(query, (username)).fetchone()
+    
+    if(pengguna is None):
+        raise Exception("Username dimasukkan tidak terdaftar!")
+    
+    dictPengguna = {
+        "id_pengguna": pengguna[0],
+        "username": pengguna[1],
+        "password": pengguna[2],
+        "role": pengguna[3],
+    }
+    
+    return dictPengguna
