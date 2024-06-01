e52beff4d10d4150b20822bcb620621e67030f30
christianHadinata <christianhadinata2004@gmail.com>
2024-06-01 23:50:41 +0700

    add transaction feature

--- services/transaksi.py
+++ services/transaksi.py
@@ -1,10 +1,17 @@
 def insertTransaksi(id_pengguna, id_furnitur, cursor=None):
-    query = '''INSERT INTO transaksi (id_pengguna, id_furnitur) VALUES (?, ?)'''
+    query = '''INSERT INTO Transaksi (id_pengguna, id_furnitur)
+    OUTPUT INSERTED.id_transaksi
+    VALUES (?, ?)'''
 
     cursor.execute(query, (id_pengguna, id_furnitur))
 
-def insertManyTransaksiBagianFurnitur(id_transaksi,transaksiBagianFurniturData,cursor=None) : 
-    query = '''INSERT INTO (id_transaksi, id_bagian_furnitur, id_warna, id_material, kuantitas)
+    id_transaksi = cursor.fetchone()[0]
+
+    return {"id_transaksi": id_transaksi}
+
+
+def insertManyTransaksiBagianFurnitur(id_transaksi, transaksiBagianFurniturData, cursor=None):
+    query = '''INSERT INTO Transaksi_Bagian_Furnitur(id_transaksi, id_bagian_furnitur, id_warna, id_material, kuantitas)
     VALUES '''
 
     colValues = []
@@ -15,7 +22,7 @@ def insertManyTransaksiBagianFurnitur(id_transaksi,transaksiBagianFurniturData,c
         colValues.append(data.get("id_warna"))
         colValues.append(data.get("id_material"))
         colValues.append(data.get("kuantitas"))
-    
+
     query = query.rstrip(", ")
 
     cursor.execute(query, colValues)
